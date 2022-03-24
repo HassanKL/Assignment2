8d7e637d7c0fff93fb6e59d8455925eff5f6281d
HassanKL <91002826+HassanKL@users.noreply.github.com>
2022-03-24 16:10:11 -0400

    Add files via upload

---
+++ homework2/app/app.js
@@ -0,0 +1,17 @@
+function initListeners() {
+    $(".bars").click(function(e){
+        $(".bars").toggleClass("active")
+        $(".links").toggleClass("active")
+    });
+}
+
+$(".links a").click(function(e){
+    $(".bars").toggleClass("active")
+    $(".links").toggleClass("active")
+});
+
+
+
+$(document).ready(function () {
+    initListeners();
+});
